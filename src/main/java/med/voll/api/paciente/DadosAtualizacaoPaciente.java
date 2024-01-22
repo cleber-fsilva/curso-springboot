4fbff67ce3e3cf11886ca37f8c83c197f58681fb
Cleber Ferreira Da Silva <c.silva@perkons.com>
2024-01-22 20:08:45 -0300

    primeiro commit com API já funcional

---
+++ src/main/java/med/voll/api/paciente/DadosAtualizacaoPaciente.java
@@ -0,0 +1,18 @@
+package med.voll.api.paciente;
+
+import jakarta.validation.Valid;
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosAtualizacaoPaciente (
+
+        Long id,
+
+        String nome,
+
+        String telefone,
+
+        @Valid
+        DadosEndereco endereco
+){
+
+}
